a85b2ba2cb9c9403c596d2ae2018468e31acd89d
CattoElectrico <lucas8422olmedoj@gmail.com>
2026-04-06 15:03:42 -0400

    add user entity, first step to the jwt token and identifier

---
+++ MyMDentistComerce/src/main/java/com/MyMDentis/MyMDentistComerce/Model/UserEntity.java
@@ -0,0 +1,26 @@
+package com.MyMDentis.MyMDentistComerce.Model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Entity
+@Table(name = "entity_user")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user", length = 7, nullable = false, unique = true)
+    private Long idUser;
+    @Column(name = "name_user", length = 100, nullable = false, unique = false)
+    private String nameUser;
+    @Column(name = "email_user", length = 100, nullable = false, unique = true)
+    private String emailUser;
+    @Column(name = "password_user", length = 50, nullable = false, unique = false)
+    private String passwordUser;
+    @Column(name = "role_user", length = 20, nullable = false, unique = false)
+    private Roles role;
+}
---
+++ MyMDentistComerce/src/main/java/com/MyMDentis/MyMDentistComerce/Repository/UserEntityRepository.java
@@ -0,0 +1,14 @@
+package com.MyMDentis.MyMDentistComerce.Repository;
+
+import com.MyMDentis.MyMDentistComerce.Model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
+
+    Optional<UserEntity> findByNameUser(String nameUser);
+
+}
